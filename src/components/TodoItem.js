ffeb3a0f334527acb5045eceb62290215a496215
Sebastian Nabrink <sebastian.nabrink@hotmail.com>
2016-10-25 15:56:13 +0200

    Added checkbox to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,22 @@ import React from 'react';
 class TodoItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      done: false
+    };
+
+    this.onChecked = this.onChecked.bind(this);
+  }
+
+  onChecked(e) {
+    this.setState({
+      done: e.target.checked
+    });
   }
 
   render() {
-    return <div>{this.props.item.description}</div>
+    return <div><input type="checkbox" onChange={this.onChecked} />{this.props.item.description}</div>
   }
 }
 
